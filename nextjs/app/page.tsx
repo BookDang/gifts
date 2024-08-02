93be7eb89dc287dafa556058d3ba2a9c4752e80a
Book Dang <sachdang.dev@gmail.com>
2024-08-03 00:59:38 +0700

    Change CMD run start the nestjs project.

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { AiFillAlert } from "react-icons/ai"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello s
+      <AiFillAlert className="text-5xl text-red-500" />
     </main>
   );
 }
