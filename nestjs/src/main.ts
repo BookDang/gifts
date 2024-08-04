4d9e5f36a1f3c39d2ba4e34e7de9e1247c4e2100
Book Dang <sachdang.dev@gmail.com>
2024-08-04 23:58:15 +0700

    Dockerize nginx. NGINX Reverse Proxy Configuration.

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3003);
+  // await app.listen(3003);
+  app.setGlobalPrefix('api');
+  await app.listen(process.env.APP_PORT || 3003, '0.0.0.0');
 }
 bootstrap();
