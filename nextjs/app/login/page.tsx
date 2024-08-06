bbf57bff33f7618c7b6f8ca96c1d74041c61c5a4
Book Dang <sachdang.dev@gmail.com>
2024-08-06 23:00:16 +0700

    chore: Remove empty login form from login page

--- nextjs/app/login/page.tsx
+++ nextjs/app/login/page.tsx
@@ -3,9 +3,6 @@
 import React from 'react'
 
 const LoginPage: React.FC = () => {
-
-  
-
   return (
     <div className="login-container bg-login-bg-gradient w-screen h-screen relative">
       <div className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white absolute shadow-md rounded-xl w-full max-w-md">
@@ -13,9 +10,7 @@ const LoginPage: React.FC = () => {
           <div className="form-title rounded-t-xl bg-amber-500 py-5">
             <h1 className="text-2xl font-bold text-center text-white">Login</h1>
           </div>
-          <div className="login-form px-4 py-6">
-
-          </div>
+          <div className="login-form px-4 py-6"></div>
         </div>
       </div>
     </div>
