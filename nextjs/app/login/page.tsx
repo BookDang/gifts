928510942c3ffcdf0c9888dd6e9838d2f402951b
Book Dang <sachdang.dev@gmail.com>
2024-08-17 20:37:00 +0700

    Update login page layout to full screen width and height

--- nextjs/app/login/page.tsx
+++ nextjs/app/login/page.tsx
@@ -10,7 +10,7 @@ const LoginPage: React.FC = () => {
   const t = useTranslations('LoginPage')
 
   return (
-    <div className="relative">
+    <div className="w-full h-screen relative">
       <div
         className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 absolute
        bg-white shadow-md rounded-t-xl w-full max-w-md"
