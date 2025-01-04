d48857fbd06600a2e612ceb0ab08cd347b2d6aa6
DangSach <sachdang.dev@gmail.com>
2025-01-04 21:15:51 +0700

    feat: Connect mysql and remove all symbol ; in the nestjs project.

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from '@/app.controller'
+import { AppService } from '@/app.service'
+import { UsersModule } from '@/users/users.module'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'root',
+      password: 'root_password_giftsdb',
+      database: 'giftsdb',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
