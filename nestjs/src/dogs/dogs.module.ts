de4ba4a241a896ea8a5aca489b475823e7408d33
DangSach <sachdang.dev@gmail.com>
2025-01-17 19:49:44 +0700

    feat: Remove FishService import from DogsModule.

--- nestjs/src/dogs/dogs.module.ts
+++ nestjs/src/dogs/dogs.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { DogsService } from './dogs.service';
 import { DogsController } from './dogs.controller';
-import { FishService } from 'src/cats/fish.service';
 import { CatsModule } from 'src/cats/cats.module';
 
 @Module({
