b9e94abd164c32e9d20741d31db2d5521e01f748
DangSach <sachdang.dev@gmail.com>
2025-01-24 11:41:24 +0700

    refactor: update import paths and consolidate user constants.

--- nestjs/src/utils/constants/user.const.ts
+++ nestjs/src/utils/constants/user.const.ts
@@ -1,5 +1,3 @@
-export const GENDER = [
-  'male',
-  'female',
-  'other'
-] as const;
+export const GENDER = ['male', 'female', 'other'] as const
+
+export const USER_TYPEs = ['admin', 'user'] as const
